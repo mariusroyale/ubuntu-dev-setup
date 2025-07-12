02fc6a69720a751e791076848b1ab5081ae04141
Marius Iordache <marius.technical.leader@gmail.com>
2025-07-11 22:50:41 -0400

    changed the verbiage

--- fix-apps/davinci-studio.sh
+++ fix-apps/davinci-studio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo mkdir /opt/resolve/libs/unneeded
-sudo mv /opt/resolve/libs/libglib* /opt/resolve/libs/unneeded/
-sudo mv /opt/resolve/libs/libgio* /opt/resolve/libs/unneeded/
-sudo mv /opt/resolve/libs/libgmodule* /opt/resolve/libs/unneeded/
+sudo mkdir /opt/resolve/libs/deprecated
+sudo mv /opt/resolve/libs/libglib* /opt/resolve/libs/deprecated/
+sudo mv /opt/resolve/libs/libgio* /opt/resolve/libs/deprecated/
+sudo mv /opt/resolve/libs/libgmodule* /opt/resolve/libs/deprecated/
